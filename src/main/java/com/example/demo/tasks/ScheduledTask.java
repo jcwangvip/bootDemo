436978d263eedc1dfda11c9d9df63fe59fc65719
jcwang <451809860@qq.com>
2017-12-15 15:42:20 +0800

    测试git,第一次提交

---
+++ src/main/java/com/example/demo/tasks/ScheduledTask.java
@@ -0,0 +1,33 @@
+package com.example.demo.tasks;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:quartz.properties")
+public class ScheduledTask{
+	
+	
+
+  @Scheduled(fixedRate = 5000)
+  public void scheduledTask(){
+    
+	  System.out.println("sdf----------");
+  }
+  
+  @Scheduled(cron = "0 */1 *  * * * ")
+  public void scheduledTaskOrCron(){
+    
+	  System.out.println("gogogo----------");
+  }
+  
+  @Scheduled(cron="${jobs.schedule}")
+  public void scheduledTaskOrCron1(){
+    
+	  System.out.println("12345y678----------");
+  }
+}
